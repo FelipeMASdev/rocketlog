d578654276f291e58ccd91f5c2755be7eb02ec00
FelipeMASdev <felipe.mas.dev@gmail.com>
2026-04-13 14:37:26 -0300

    user creation route

--- src/controllers/users-controller.ts
+++ src/controllers/users-controller.ts
@@ -1,10 +1,49 @@
 import { Request, Response } from 'express';
+import { prisma } from "@/database/prisma";
+import { hash } from 'bcrypt';
+import { z } from 'zod';
+import { AppError } from '@/utils/AppError';
 
 class UsersController {
 
-  create(req: Request, res: Response) {
-    return res.json({ message: 'User created successfully' });
+  async create(req: Request, res: Response) {
+    //validating the request body using zod
+    const bodySchema = z.object({
+      name: z.string().trim().min(2),
+      email: z.string().email(),
+      password: z.string().min(6),
+    });
+
+    const { name, email, password} = bodySchema.parse(req.body);
+
+    //validating if there is no user with the same email in the database
+    const userWithSameEmail = await prisma.user.findFirst({
+      where: { email },
+    });
+
+    if (userWithSameEmail) {
+      throw new AppError("User with this email already exists");
+    }
+
+    //password cryptography
+    const hashedPassword = await hash(password, 8);
+
+    //creating the user in the database
+    const user = await prisma.user.create({
+      data: {
+        name,
+        email,
+        password: hashedPassword,
+      }
+    });
+
+    //destructuring the user object to remove the password field
+    const { password: _, ...userWithoutPassword } = user;
+
+    //returning the created user
+    return res.status(201).json( userWithoutPassword );
   }
+
 }
 
 export { UsersController };
