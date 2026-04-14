583e96352001c7d8fed2b3675558e0b9ee538203
FelipeMASdev <felipe.mas.dev@gmail.com>
2026-04-14 07:49:30 -0300

    updating delivery status

---
+++ src/controllers/deliveries-status-controller.ts
@@ -0,0 +1,30 @@
+import { Request, Response } from "express";
+import { prisma } from "@/database/prisma";
+import { z } from "zod";
+
+class DeliveriesStatusController {
+  async update(req: Request, res: Response) {
+    // Validate request parameters and body
+    const paramsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const bodySchema = z.object({
+      status: z.enum(["processing", "shipped", "delivered"]),
+    });
+
+    const { id } = paramsSchema.parse(req.params);
+    const { status } = bodySchema.parse(req.body);
+
+    // Update the delivery status in the database
+    await prisma.delivery.update({
+      data: { status },
+      where: { id },
+    });
+
+    // Return a success response
+    return res.json({ message: "Delivery status updated successfully." });
+  }
+}
+
+export { DeliveriesStatusController };
